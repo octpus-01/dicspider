9f02169be2455d440b6f02fc7a5fea2fa30f8125
octopus_pierre <octopus_pierre@163.com>
2025-02-25 22:37:12 +0800

    清理一些无关文件，调整当前程序

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from spider_part import find_everything
 
-words = ['challenge']
+type_in = ''
+words = []
+while type_in != '0' :
+   
+    type_in = str(input('enter the words or 0 to stop: '))
+    words.append(type_in)
+
+words.remove('0')
+    
+
+print(words)
+
 
 for i in words:
     index_i = str(words.index(i))
+
     print('----------------------------------'+index_i+'----------------------------------')
+
 
     print(find_everything(i))
